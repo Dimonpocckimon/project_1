0465f424794c9db103d823e091d9fb5502316192
Владислав Чевеленков <vladislavcevelenkov@MacBook-Air-Vladislav.local>
2024-03-24 14:01:02 +0300

    thirst commit for python

--- python_web.py
+++ python_web.py
@@ -1,3 +1,27 @@
-print('Hello world')
-print('Goodbye world')
-print('why goodbye?')
+from flask import Flask, session, redirect, url_for
+from random import randint
+from sql import get_question
+
+def choose_quiz():
+    global num_quiz, num_quest
+    num_quiz = randint(1, 3)
+    num_quest = 0
+    return '<a href="/test">перейти в тест</a>'
+
+def test():
+    global num_quest
+    result = get_question(num_quest, num_quiz)
+    if result is None or len(result) == 0:
+        redirect(url_for('results'))
+    else:
+        num_quest = result[0]
+    return f'<h1>{str(result)}</h1>1'
+
+def results():
+    pass
+
+app = Flask(__name__)
+app.add_url_rule('/', 'choose_quiz', choose_quiz)
+app.add_url_rule('/test', 'test', test)
+app.add_url_rule('/results', 'results', results)
+app.run()
