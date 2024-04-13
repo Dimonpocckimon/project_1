bf6262b0f646b67813830c496666252b67b11cbe
Владислав Чевеленков <vladislavcevelenkov@MacBook-Air-Vladislav.local>
2024-04-13 19:01:23 +0300

    ENTER TEXT

--- python_web.py
+++ python_web.py
@@ -11,15 +11,15 @@ def choose_quiz():
         peremennay = request.form.get('quizes')
         session['quiz'] = peremennay
         session['question'] = 0
-        redirect(url_for('test'))
+        return redirect(url_for('test'))
+    
 
 def test():
-    global num_quest
-    result = get_question(num_quest, num_quiz)
+    result = get_question(session['question'], session['quiz'])
     if result is None or len(result) == 0:
         redirect(url_for('results'))
     else:
-        num_quest = result[0]
+        session['question'] = result[0]
     return f'<h1>{str(result)}</h1>1'
 
 def results():
@@ -27,6 +27,7 @@ def results():
 folder = os.getcwd()
 
 app = Flask(__name__, template_folder=folder, static_folder=folder)
+app.config['SECRET_KEY'] = 'password'
 app.add_url_rule('/', 'choose_quiz', choose_quiz, methods = ['post', 'get'])
 app.add_url_rule('/test', 'test', test, methods = ['post', 'get'])
 app.add_url_rule('/results', 'results', results, methods = ['post', 'get'])
